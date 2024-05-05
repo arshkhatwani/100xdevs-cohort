598495839ec6cc315efbf7eb8ad8832ee34466fd
arshkhatwani <arsh.khatwani@gmail.com>
2024-05-05 11:55:03 +0530

    add Partial

--- more-typescript-apis/index.ts
+++ more-typescript-apis/index.ts
@@ -7,8 +7,14 @@ interface User {
 
 type UserProfile = Pick<User, "name" | "email" | "age">;
 
+type UpdateProps = Partial<User>;
+
 function displayUserProfile(user: UserProfile) {
     console.log(user);
 }
+// displayUserProfile({ name: "John", email: "a@a.com", age: 30 });
 
-displayUserProfile({ name: "John", email: "a@a.com", age: 30 });
+function updateUser(updatedProps: UpdateProps) {
+    console.log(updatedProps);
+}
+updateUser({ name: "John", email: "a@a.com", createdAt: new Date() });
